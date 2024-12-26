dfbc27f4981324aa855e4f76f65dba4f66a8a36e
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-12-26 11:25:53 +0530

    folder

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ const socket = require('socket.io');
 const http = require('http');
 const { Chess } = require('chess.js');
 
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
